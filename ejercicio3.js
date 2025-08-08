d8863bd1188e709f0f286ce88d1cef69680257e2
Danielrzzz <daniel.rodriguez2999@gmail.com>
2025-08-08 19:27:46 +0200

    ejercicio3.finalizado

---
+++ ejercicio3.js
@@ -0,0 +1,53 @@
+const users = [
+  {
+    name: "Alberto",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 50 },
+      rain: { format: "ogg", volume: 60 },
+      firecamp: { format: "mp3", volume: 80 },
+    },
+  },
+  {
+    name: "Antonio",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 30 },
+      shower: { format: "ogg", volume: 55 },
+      train: { format: "mp3", volume: 60 },
+    },
+  },
+  {
+    name: "Santiago",
+    favoritesSounds: {
+      shower: { format: "mp3", volume: 50 },
+      train: { format: "ogg", volume: 60 },
+      firecamp: { format: "mp3", volume: 80 },
+    },
+  },
+  {
+    name: "Laura",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 67 },
+      wind: { format: "ogg", volume: 35 },
+      firecamp: { format: "mp3", volume: 60 },
+    },
+  },
+];
+
+
+function favoritesSounds(users) {
+
+    let count = {};
+
+    for (let user of users) {
+        for(let data in user.favoritesSounds) {
+            if (count[data]) {
+                count[data] = count[data] + 1;
+            } else {
+                count[data] = 1;
+            }
+        }
+    }
+    return count;
+}
+
+console.log(favoritesSounds(users));
